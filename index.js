de600593b0897e8a93f43407335994c19183dc68
Raghuwansh Kumar Singh <wansh786@gmail.com>
2023-12-11 10:21:39 +0530

    done

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express=require("express")
 const {connection}=require("./db")
 const {userRouter}=require("./routes/user.routes")
 const {noteRouter}=require("./routes/note.routes")
+const cors=require("cors")
 
 const app=express();
 
 app.use(express.json())
+app.use(cors())
 
 app.use("/users",userRouter)
 app.use("/notes",noteRouter)
